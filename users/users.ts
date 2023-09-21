24b90ee6853a2a04e0f0570ece64563138f03de0
Alexandr Chubukov <achubukov@htc-cs.ru>
2023-09-21 08:32:53 +0400

    Start of the project

---
+++ users/users.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+export const router = express.Router()
+
+router.post('/login', (req, res) => {
+  res.send('login')
+})
+
+router.post('/register', (req, res) => {
+  res.send('register')
+})
